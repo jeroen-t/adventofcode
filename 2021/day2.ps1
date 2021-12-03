3948ddbda117f0e3344ee0fffb433099accb2fa0
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-03 20:11:57 +0100

    typo, mb

--- 2021/day2.ps1
+++ 2021/day2.ps1
@@ -1,7 +1,7 @@
 function Get-aocSubmarineDirections {
     Param (
         [parameter(Mandatory)]
-        [string]$course
+        [string[]]$course
     )
     Process {
         $Directions = $course -split '\n' | ForEach-Object {
@@ -76,6 +76,6 @@ function Move-aocSubmarine {
 $path = Join-Path $PSScriptRoot ..\input\2021\input_day2.txt
 $course = Get-Content -Path $path
 
-$location1 = (Get-aocSubmarineDirection $course | Move-aocSubmarine).Location
-$location2 = (Get-aocSubmarineDirection $course | Move-aocSubmarine -IncludeAim).Location
+$location1 = (Get-aocSubmarineDirections $course | Move-aocSubmarine).Location
+$location2 = (Get-aocSubmarineDirections $course | Move-aocSubmarine -IncludeAim).Location
 "[Day 2] first answer: {0}, second answer: {1} " -f $location1,$location2
