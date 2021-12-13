bf07e21659bf02632be9510e9ea0d768514aa98c
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-13 11:41:54 +0100

    switch Verb/Noun(s)

--- 2021/template/dayX.Tests.ps1
+++ 2021/template/dayX.Tests.ps1
@@ -2,18 +2,18 @@ BeforeAll {
     . $PSCommandPath.Replace('.Tests.ps1','.ps1')
 }
 
-describe 'Get-aocVerb' {
+describe 'Verb-aocNoun' {
     BeforeAll {
         $data = import-aocData -day X -dummy
     }
     Context 'Does something' {
-        it 'Returns X' {
-            Get-aocVerb $data | should -Be X
+        it 'Does X' {
+            Verb-aocNoun $data | should -Be X
         }
     }
     Context 'Does something' {
-        it 'Returns X' {
-            Get-aocVerb $data | should -Be X
+        it 'Does X' {
+            Verb-aocNoun $data | should -Be X
         }
     }
 }
