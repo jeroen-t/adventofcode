9233c6b6e5566c532abb087764e197237c23cfdf
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-13 12:04:14 +0100

    day 11 - skeleton

---
+++ 2021/day11.ps1
@@ -0,0 +1,18 @@
+function Import-aocData ([int]$day,[switch]$dummy) {
+    if ($dummy.IsPresent) {
+        $path = Join-Path $PSScriptRoot "..\input\2021\input_day$day`_dummy.txt"
+        $data = Get-Content $path
+    } else {
+        $path = Join-path $PSScriptRoot "..\input\2021\input_day$day`.txt"
+        $data = Get-Content $path
+    }
+    Write-Output $data
+}
+
+$X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
+$data = Import-aocData -day $X -dummy
+
+$a1=$null
+$a2=$null
+
+"[Day $X] My answer for part 1 is: {0}, and for part 2 is: {1}." -f $a1,$a2
