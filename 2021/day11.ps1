7f23abe54c4e0e032c51c249bcc58ee3ac1bacfb
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-14 16:44:42 +0100

    temp

--- 2021/day11.ps1
+++ 2021/day11.ps1
@@ -1,3 +1,5 @@
+$totalflashes = $steps = $flashes = 0
+
 function Import-aocData ([int]$day,[switch]$dummy) {
     if ($dummy.IsPresent) {
         $path = Join-Path $PSScriptRoot "..\input\2021\input_day$day`_dummy.txt"
@@ -9,46 +11,181 @@ function Import-aocData ([int]$day,[switch]$dummy) {
     Write-Output $data
 }
 
+function Get-aocDumboStartState ([string[]]$dumbos) {
+    $octopus = [ordered]@{}
+    for ($y = 0; $y -lt $dumbos.Count; $y++) {
+        for ($x = 0; $x -lt $dumbos[0].Length; $x++) {
+            $coord = "$x,$y"
+            $octopus[$coord] = [int]$dumbos[$y][$x] - 48
+        }
+    }
+    $octopus
+}
+
+function Get-aocDumboNeighbours ([string]$coord) {
+    $neighbours = @{}
+    if ($neighbours.ContainsKey($coord)) {
+        $neighbours[$coord]
+    } else {
+        [int]$x, [int]$y = $coord -split ','
+        $nb = for ($i = -1; $i -lt 2; $i++) {
+            for ($j = -1; $j -lt 2; $j++) {
+                if ($x + $i -ge 0 -and $y + $j -ge 0 -and $x + $i -lt $dumbos[0].Length -and $y + $j -lt $dumbos.Count -and -not ($i -eq 0 -and $j -eq 0)) {
+                    "{0},{1}" -f $($x + $i), $($y + $j)
+                }
+            }
+        }
+        $neighbours[$coord] = $nb
+        $nb
+    }
+}
+
+Get-aocDumboNeighbours "0,0"
+
+function New-aocDumboFlash ([string]$coord) {
+    $script:flashes ++
+    $octopus[$coord] = 0
+    Get-aocDumboNeighbours $coord | ForEach-Object {
+        $octo = $_
+        switch ($octopus[$_]) {
+            ({$_ -eq 0})    { continue              }
+            ({$_ -ge 9})    { New-aocDumboFlash $octo    }
+            default         { $octopus[$octo]++     }
+        }
+    }
+}
+
+function Start-aocDumboEnergyIncrease {
+    $script:steps ++
+    for ($y = 0; $y -lt $dumbos.Count; $y++) {
+        for ($x = 0; $x -lt $dumbos[0].Length; $x++) {
+            $octopus["$x,$y"]++
+        }
+    }
+}
+
+function Step-aocDumboGrid ([int]$days) {
+    for ($d = 0; $d -lt $days; $d++) {
+        Start-aocDumboEnergyIncrease
+        for ($y = 0; $y -lt $dumbos.Count; $y++) {
+            for ($x = 0; $x -lt $dumbos[0].Length; $x++) { 
+                if ($octopus["$x,$y"] -gt 9) {
+                    New-aocDumboFlash "$x,$y"
+                }
+            }
+        }
+    }
+    $script:totalflashes += $script:flashes
+}
+
 $X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
 $data = Import-aocData -day $X -dummy
 
-$border = @('X' * 10)
-$data += $border
+$octopus = Get-aocDumboStartState $data
 
-$array = $border + $data
-$array = foreach ($row in $array) {
-    'X' + $row + 'X'
-}
+# part 1
+Step-aocDumboGrid 100
+"After step $steps there have been $totalflashes flashes"
 
 <#
+# part 2
+do {
+    $flashes = 0
+    Step-aocDumboGrid 1
+} until ($flashes -eq $octopus.count)
 
-XXXXXXXXXXXX
-X5483143223X
-X2745854711X
-X5264556173X
-X6141336146X
-X6357385478X
-X4167524645X
-X2176841721X
-X6882881134X
-X4846848554X
-X5283751526X
-XXXXXXXXXXXX
+"After step $step all dumbos have flashed simultaneously"
 
-1..10
-
-Each step..
-- Energy level of each octopus +1
-- Energy level -gt 9 { FLASH! and adjacent octopus +1. If adjacent octopus -gt 9 also FLASH! loops til all are done (octopus flashes once per step)}
-- if octopus flashes then its energy is set to 0 as all its energy is used
-
-
-how many flashes are there after 100 steps?
-#>
-
-$array[1][10]
-
-$a1=$null
-$a2=$null
+# $a1=$null
+# $a2=$null
 
 "[Day $X] My answer for part 1 is: {0}, and for part 2 is: {1}." -f $a1,$a2
+
+
+
+
+
+
+
+<#
+function Get-aocDumboNeighbours ([string]$coord) {
+    $neighbours = @{}
+    if ($neighbours.ContainsKey($coord)) {
+        $neighbours[$coord]
+    } else {
+        [int]$x, [int]$y = $coord -split ','
+        $nb = for ($i = -1; $i -lt 2; $i++) {
+            for ($j = -1; $j -lt 2; $j++) {
+                if ($x + $i -ge 0 -and $y + $j -ge 0 -and $x + $i -lt $dumbos[0].Length -and $y + $j -lt $dumbos.Count -and -not ($i -eq 0 -and $j -eq 0)) {
+                    "{0},{1}" -f $($x + $i), $($y + $j)
+                }
+            }
+        }
+        $neighbours[$coord] = $nb
+        $nb
+    }
+}
+
+function New-aocDumboFlash ([string]$coord) {
+    $script:flashes ++
+    $octopus[$coord] = 0
+    Get-aocDumboNeighbours $coord | ForEach-Object {
+        $octo = $_
+        switch ($octopus[$_]) {
+            ({$_ -eq 0})    { continue              }
+            ({$_ -ge 9})    { New-aocDumboFlash $octo    }
+            default         { $octopus[$octo]++     }
+        }
+    }
+}
+
+function Start-aocDumboEnergyIncrease {
+    $script:steps ++
+    for ($y = 0; $y -lt $dumbos.Count; $y++) {
+        for ($x = 0; $x -lt $dumbos[0].Length; $x++) {
+            $octopus["$x,$y"]++
+        }
+    }
+}
+
+function Step-aocDumboGrid ([int]$days) {
+    for ($d = 0; $d -lt $days; $d++) {
+        Start-aocDumboEnergyIncrease
+        for ($y = 0; $y -lt $dumbos.Count; $y++) {
+            for ($x = 0; $x -lt $dumbos[0].Length; $x++) { 
+                if ($octopus["$x,$y"] -gt 9) {
+                    New-aocDumboFlash "$x,$y"
+                }
+            }
+        }
+    }
+    $script:totalflashes += $script:flashes
+}
+
+$X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
+$data = Import-aocData -day $X -dummy
+
+$octopus = Get-aocDumboStartState $data
+$octopus | gm
+
+# part 1
+Step-aocDumboGrid 100
+"After step $steps there have been $totalflashes flashes"
+
+<#
+# part 2
+do {
+    $flashes = 0
+    Step-aocDumboGrid 1
+} until ($flashes -eq $octopus.count)
+
+"After step $step all dumbos have flashed simultaneously"
+
+# $a1=$null
+# $a2=$null
+
+"[Day $X] My answer for part 1 is: {0}, and for part 2 is: {1}." -f $a1,$a2
+#>
+
+
+#>
