a6bf686912dad3e84894a640af2de83093fd3807
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-15 12:52:24 +0100

    day 12 - p1, issues /w scope if using functions

--- 2021/day12.Tests.ps1
+++ 2021/day12.Tests.ps1
@@ -4,11 +4,13 @@ BeforeAll {
 
 describe 'Verb-aocNoun' {
     BeforeAll {
-        $data = import-aocData -day X -dummy
+        $data = import-aocData -day 12 -dummy
+        $edges = Get-aocEdges $data
+        $result = Get-aocPossiblePaths $edges
     }
     Context 'Does something' {
         it 'Does X' {
-            Verb-aocNoun $data | should -Be X
+            $result | should -Be 10
         }
     }
     Context 'Does something' {
--- 2021/day12.ps1
+++ 2021/day12.ps1
@@ -1,6 +1,20 @@
-function Import-aocData ([int]$day,[switch]$dummy) {
+function Import-aocData {
+    Param (
+        [int]$day,
+        
+        [switch]$dummy,
+        
+        [ValidateSet(1,2,3)]
+        [int]$sample = 1
+    )
     if ($dummy.IsPresent) {
-        $path = Join-Path $PSScriptRoot "..\input\2021\input_day$day`_dummy.txt"
+        switch($sample)
+        {   
+            1 { $n = $null  }
+            2 { $n = 2      }
+            3 { $n = 3      }
+        }
+        $path = Join-Path $PSScriptRoot "..\input\2021\input_day$day`_dummy$n`.txt"
         $data = Get-Content $path
     } else {
         $path = Join-path $PSScriptRoot "..\input\2021\input_day$day`.txt"
@@ -9,18 +23,72 @@ function Import-aocData ([int]$day,[switch]$dummy) {
     Write-Output $data
 }
 
+function Get-aocEdges ([string[]]$data){
+    $edges = @{}
+    foreach ($line in $data) {
+        $src,$dest = $line.Split('-')
+        if ($edges[$src]) {
+            $edges.$src += $dest
+        } elseif ($src) {
+            $edges.$src= @()
+            $edges.$src += $dest
+        }
+        if ($edges[$dest]) {
+            $edges.$dest += $src
+        } else {
+            $edges.$dest = @()
+            $edges.$dest += $src
+        }
+        $edges.Remove('end')
+    }
+    $edges
+}
+
 $X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
 $data = Import-aocData -day $X -dummy
 
-foreach ($line in $data) {
-    $start,$end = $line.Split('-')
-    $start
-    $end
-    " "
+$edges = Get-aocEdges $data
+
+#$allpaths = @()
+$allpaths = 0
+$todo = New-Object System.Collections.Queue
+$todo.Enqueue(@('start'))
+while($todo.Count -ne 0) {
+    $path = $todo.Dequeue()
+
+    if ($path[-1] -eq 'end') {
+        #$allpaths += $path
+        $allpaths ++
+        continue
+    }
+
+    foreach ($cand in $edges[$path[-1]]) {
+        if (-not($cand -ceq $cand.ToLower()) -or -not($cand -in $path)) {
+            $todo.Enqueue($path + @($cand))
+        }
+    }
 }
+$a1 = $allpaths
 
+# $allpaths = 0
+# $todo = New-Object System.Collections.Queue
+# $todo.Enqueue(@('start'))
+# while($todo.Count -ne 0) {
+#     $path = $todo.Dequeue()
 
-$a1=$null
-$a2=$null
+#     if ($path[-1] -eq 'end') {
+#         #$allpaths += $path
+#         $allpaths ++
+#         continue
+#     }
+
+#     foreach ($cand in $edges[$path[-1]]) {
+#         if (-not($cand -ceq $cand.ToLower()) -or -not($cand -in $path)) {
+#             $todo.Enqueue($path + @($cand))
+#         }
+#     }
+# }
+# $a2 = $allpaths
+
 
 "[Day $X] My answer for part 1 is: {0}, and for part 2 is: {1}." -f $a1,$a2
