faf618b27a6fba5ac942fe7de37808c991b388a3
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-15 21:55:37 +0100

    add to import function

--- 2021/day13.ps1
+++ 2021/day13.ps1
@@ -1,16 +1,27 @@
 function Import-aocData ([int]$day,[switch]$dummy) {
     if ($dummy.IsPresent) {
         $path = Join-Path $PSScriptRoot "..\input\2021\input_day$day`_dummy.txt"
-        $data = Get-Content $path
     } else {
         $path = Join-path $PSScriptRoot "..\input\2021\input_day$day`.txt"
-        $data = Get-Content $path
     }
-    Write-Output $data
+    $data,$instructions = (Get-Content $path -raw) -split "\r\n\r\n"
+    [PSCustomObject]@{
+        data = -split($data)
+        instructions = $instructions -split "\r\n"
+    }
 }
 
 $X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
 $data = Import-aocData -day $X -dummy
+
+foreach ($line in $data.data) {
+    "hello $line"
+}
+
+
+foreach ($line in $data.instructions) {
+    "hello $line"
+}
 
 $a1=$null
 $a2=$null
