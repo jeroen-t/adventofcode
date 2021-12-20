c63c1d1a2d9022f134cf7d4b039d7536b47b0edf
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-20 21:31:23 +0100

    Day 15 - pt 2

--- 2021/day15.Tests.ps1
+++ 2021/day15.Tests.ps1
@@ -2,18 +2,18 @@ BeforeAll {
     . $PSCommandPath.Replace('.Tests.ps1','.ps1')
 }
 
-describe 'Verb-aocNoun' {
+describe 'Get-aocSafestPath' {
     BeforeAll {
-        $data = import-aocData -day X -dummy
+        $data = import-aocData -day 15 -dummy
     }
-    Context 'Does something' {
-        it 'Does X' {
-            Verb-aocNoun $data | should -Be X
+    Context 'Calculates the lowest total risk of any path form the top left to the bottom right of the cave' {
+        it 'Returns a total risk of 40' {
+            Get-aocSafestPath $data | should -Be 40
         }
     }
-    Context 'Does something' {
-        it 'Does X' {
-            Verb-aocNoun $data | should -Be X
+    Context 'Calculates the lowest total risk of any path form the top left to the bottom right of the cave using the full map' {
+        it 'Returns a total risk of 315' {
+            Get-aocSafestPath $data -EntireCave | should -Be 315
         }
     }
 }
--- 2021/day15.ps1
+++ 2021/day15.ps1
@@ -8,69 +8,73 @@ function Import-aocData ([int]$day,[switch]$dummy) {
     Write-Output $data
 }
 
-$X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
-$data = Import-aocData -day $X #-dummy
-
-# visualization
-
-for ($r = 0; $r -lt $data.Count; $r++) {
-    for ($c = 0; $c -lt $data[0].Length; $c++) {
-        Write-host $data[$r][$c] -NoNewline
+function New-aocGrid ($data) {
+    $grid = New-Object 'int[,]' $data.Count,$data[0].Length
+    $weights = New-Object 'int[,]' $data.Count,$data[0].Length
+    for ($r = 0; $r -lt $data.Count; $r++) {
+        for ($c = 0; $c -lt $data[0].Length; $c++) {
+            [int]$grid[$r,$c] = $data[$r][$c] - 48
+            [int]$weights[$r,$c] = 0
+        }
     }
-    ""
+    $grid[0,0] = 0
+    $grid, $weights
 }
 
-# weights
-
-$weights = New-Object 'int[,]' $data.Count,$data[0].Length
-for ($r = 0; $r -lt $data.Count; $r++) {
-    for ($c = 0; $c -lt $data[0].Length; $c++) {
-        [int]$weights[$r,$c] = 0
-    }
-}
-
-#neighbors
-
-$offsets = @((0, 1),(0,-1),(1,0),(-1,0))
-$directions = @{}
-for ($r = 0; $r -lt $data.Count; $r++) {
-    for ($c = 0; $c -lt $data[0].Length; $c++) {
-        $offsets | ForEach-Object {
-            $ry,$cx = $_
-            if ($r + $ry -ge 0 -and $r + $ry -lt $data.Count -and $c + $cx -ge 0 -and $c + $cx -lt $data[0].Length) {
-                $directions["$r,$c"] += @("$($r+$ry),$($c+$cx)")
+function Get-aocNeighborVertices ($data) {
+    $offsets = @((0, 1),(0,-1),(1,0),(-1,0))
+    $directions = @{}
+    for ($r = 0; $r -lt $data.Count; $r++) {
+        for ($c = 0; $c -lt $data[0].Length; $c++) {
+            $offsets | ForEach-Object {
+                $ry,$cx = $_
+                if ($r + $ry -ge 0 -and $r + $ry -lt $data.Count -and $c + $cx -ge 0 -and $c + $cx -lt $data[0].Length) {
+                    $directions["$r,$c"] += @("$($r+$ry),$($c+$cx)")
+                }
             }
         }
     }
+    $directions
 }
 
-[string[]]$grid = $data
-$grid[0] = $grid[0] -replace "^.","0"
-$pq = New-Object System.Collections.Generic.PriorityQueue["PSObject,Int"]
-$pq.Enqueue((0,0),0)
-while ($pq) {
-    ($r, $c) = $pq.Dequeue()
-    if ("$r,$c" -notin $directions.Keys) {
-        continue
-    } else {
-        $verts = $directions["$r,$c"]
-        $directions.Remove("$r,$c")
-    }
-
-    $verts | ForEach-Object {
-        $ry,$cx = $_ -split ','
-        if (-not($weights[$ry,$cx]) -or $weights[$r,$c]+$data[$ry][$cx]-48 -lt $weights[$ry,$cx]) {
-            $weights[$ry,$cx] = $weights[$r,$c]+$data[$ry][$cx]-48
-            $pq.Enqueue(($ry,$cx), $($weights[$ry,$cx]))
+function Get-aocSafestPath ($data,[switch]$EntireCave) {
+    if ($EntireCave.IsPresent) {
+        $data = foreach($i in 0..4) {
+            $data | ForEach-Object {
+                -join $(foreach ($j in 0..4) { echo @_ | ForEach-Object {
+                    1 + ( $_ - 49 + $j + $i) % 9 }
+                })
+            }
         }
     }
-    if ($pq.Count -eq 0) {
-        break
-    }
-}
-$weights[-1,-1]
+    $grid, $weights = New-aocGrid $data
+    $directions = Get-aocNeighborVertices $data
+    $pq = New-Object System.Collections.Generic.PriorityQueue["PSObject,Int"]
+    $pq.Enqueue((0,0),0)
+    while ($pq.Count -gt 0) {
+        ($r, $c) = $pq.Dequeue()
+        if ("$r,$c" -notin $directions.Keys) {
+            continue
+        } else {
+            $verts = $directions["$r,$c"]
+            $directions.Remove("$r,$c")
+        }
 
-$a1=$null
-$a2=$null
+        $verts | ForEach-Object {
+            $ry,$cx = $_ -split ','
+            if (-not($weights[$ry,$cx]) -or $weights[$r,$c]+$grid[$ry,$cx] -lt $weights[$ry,$cx]) {
+                $weights[$ry,$cx] = $weights[$r,$c]+$grid[$ry,$cx]
+                $pq.Enqueue(($ry,$cx), $($weights[$ry,$cx]))
+            }
+        }
+    }
+    $weights[-1,-1]
+}
+
+$X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
+$data = Import-aocData -day $X #-dummy
+
+$a1 = Get-aocSafestPath $data
+$a2 = Get-aocSafestPath $data -EntireCave
 
 "[Day $X] My answer for part 1 is: {0}, and for part 2 is: {1}." -f $a1,$a2
