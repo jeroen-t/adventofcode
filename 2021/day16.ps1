186c8fa5fa0f58a45cfd16be45e840c6c86a0ea9
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2021-12-21 12:23:23 +0100

    day 16 - start pt 1

--- 2021/day16.ps1
+++ 2021/day16.ps1
@@ -9,11 +9,40 @@ function Import-aocData ([int]$day,[switch]$dummy) {
     Write-Output $data
 }
 
+function ConvertTo-aocBinary ($data) {
+    -join($data.ToCharArray() | ForEach-Object {
+        [System.Convert]::ToString([System.Convert]::ToInt32("$_", 16),2).PadLeft(4, '0')
+    })
+}
 
+$binary = ConvertTo-aocBinary $data
+$convert = { Param ($bits) [System.Convert]::ToInt32($bits, 2)}
 
-(-join($data.ToCharArray() | ForEach-Object {
-    [System.Convert]::ToString([System.Convert]::ToInt32("$_", 16),2).PadLeft(4, '0')
-})) #.substring(0,3)
+$Version = $convert.Invoke($binary.Substring(0,3))
+$Type = $convert.Invoke($binary.Substring(3,3))
+
+if ($Type -eq 4) {
+    $i = 6
+    while ($binary[$i] -eq '1') {
+        $i += 5
+    }
+    # 110100101111111000101000
+    # VVVTTTAAAAABBBBBCCCCC
+
+    # So, this packet represents a literal value with binary representation 011111100101, which is 2021 in decimal.
+    $convert.Invoke(-join($binary.Substring(6, $i-1) -split '(\w{5})' | ? {$_} | % {$_.substring(1,4)}))
+} else {
+    $i = 6
+    if ($binary[$i] -eq '0') {
+        # If the length type ID is 0, then the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
+        $convert.Invoke($binary.Substring($i+1,$i+15))
+
+    } elseif ($binary[$i] -eq '1') {
+        # If the length type ID is 1, then the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
+        $convert.Invoke($binary.Substring($i+1,$i+11))
+    }
+}
+
 
 
 $X = $($MyInvocation.MyCommand.Name).Split('.')[0] -replace "[^0-9]",''
